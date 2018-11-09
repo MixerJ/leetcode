a582075bc7e8a7d16a6789cf4ba26cc4fcb60624
MixerJ <z846204062@hotmail.com>
2018-11-09 23:50:34 +0800

    complete number 21

---
+++ sample/tests/test_nubmer_21.py
@@ -0,0 +1,15 @@
+import unittest
+
+from sample.programs.number_21 import Solution
+
+
+class TestNumber14(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_success(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
